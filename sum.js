19da7364d14a8a6bc80b403672caf1db8dbef819
pjooklas <pjooklas@users.noreply.github.com>
2022-04-15 19:22:45 +0300

    Sum Recursion solution

--- sum.js
+++ sum.js
@@ -39,6 +39,13 @@ const d = [
                     [6],
                     [7, 8],
                     [9, 10],
+                    [
+                        1, 2, 3, [4],
+                        [5],
+                        [6],
+                        [7, 8],
+                        [9, 10],
+                    ],
                 ],
             ],
         ],
@@ -52,51 +59,14 @@ function isGoodNumber(n) {
 function sum(list) {
     let total = 0;
 
-    for (const n1 of list) {
-        if (Array.isArray(n1)) {
-            for (const n2 of n1) {
-                if (Array.isArray(n2)) {
-                    for (const n3 of n2) {
-                        if (Array.isArray(n3)) {
-                            for (const n4 of n3) {
-                                if (Array.isArray(n4)) {
-                                    for (const n5 of n4) {
-                                        if (Array.isArray(n5)) {
-                                            for (const n6 of n5) {
-                                                if (!isGoodNumber(n6)) {
-                                                    continue;
-                                                }
-                                                total += n6;
-                                            }
-                                        }
-                                        if (!isGoodNumber(n5)) {
-                                            continue;
-                                        }
-                                        total += n5;
-                                    }
-                                }
-                                if (!isGoodNumber(n4)) {
-                                    continue;
-                                }
-                                total += n4;
-                            }
-                        }
-                        if (!isGoodNumber(n3)) {
-                            continue;
-                        }
-                        total += n3;
-                    }
-                }
-                if (!isGoodNumber(n2)) {
-                    continue;
-                }
-                total += n2;
-            }
+    for (const n of list) {
+        if (Array.isArray(n)) {
+            total += sum(n);
         }
-        if (!isGoodNumber(n1)) {
+        if (!isGoodNumber(n)) {
             continue;
         }
-        total += n1;
+        total += n;
     }
 
     return total;
